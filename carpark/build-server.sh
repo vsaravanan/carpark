27ff3dea9b1a7f4ebacbdd876bb1a0cc5a55f83c
saravan <saravanan.resume@gmail.com>
2020-04-01 10:25:56 +0800

    restart

--- carpark/build-server.sh
+++ carpark/build-server.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 ####
+project=carpark
 git pull
-rm -rf /opt/carpark/logs/*
+rm -rf /opt/$project/logs/*
+sudo systemctl stop $project
 mvn  -P server clean install -T 1C
-
-
+sudo systemctl restart $project; systemctl status $project
