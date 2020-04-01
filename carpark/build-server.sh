4b0ff4617506be7b67c8c224ec2ed02a9b931a8e
saravan <saravanan.resume@gmail.com>
2020-04-01 19:26:39 +0800

    permission

--- carpark/build-server.sh
+++ carpark/build-server.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 ####
 project=carpark
+git checkout .
 git pull
 rm -rf /opt/$project/logs/*
 sudo systemctl stop $project
 mvn  -P server clean install -T 1C
 sudo systemctl restart $project; systemctl status $project
+chmod 755 build-server.sh
